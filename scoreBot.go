434a9498632c61e702e0b8ccc6575ac54952053f
bobjrsenior <rcarpe31@uncc.edu>
2016-10-11 19:18:15 -0400

    Added simple ping pong discord bot feature

--- scoreBot.go
+++ scoreBot.go
@@ -13,6 +13,8 @@ import (
 	"golang.org/x/oauth2/jwt"
 	
 	sheets "google.golang.org/api/sheets/v4"
+	
+	discordgo "github.com/bwmarrin/discordgo"
 )
 
 type Record struct{
@@ -32,13 +34,11 @@ var (
 	emailFile			= flag.String("email-file", "clientemail.dat", "Developer Credential email stored in a file")
 	privateKeyFile		= flag.String("privatekey", "", "OAuth 2.0 private key")
 	applicationName = "SMB_Score_Bot"
+	discToken    = flag.String("disctoken", "dtoken.dat", "Discord token stored in a file")
+	discBotID	string
 )
 
-func initialize() {
-	flag.Parse()
-	if flag.NArg() != 0 {
-		return
-	}
+func initializeSheets() {
 
 	// Your credentials should be obtained from the Google
 	// Developer Console (https://console.developers.google.com).
@@ -67,12 +67,65 @@ func initialize() {
 	client = conf.Client(oauth2.NoContext)
 }
 
+func initializeDiscord(){
+	// Create a new Discord session using the provided login information.
+	dg, err := discordgo.New("", "", valueOrFileContents("", *discToken))
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	
+	// Get the account information.
+	u, err := dg.User("@me")
+	if err != nil {
+		fmt.Println("error obtaining account details,", err)
+	}
+
+	// Store the account ID for later use.
+	discBotID = u.ID
+	
+		// Register messageCreate as a callback for the messageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	// Simple way to keep program running until CTRL-C is pressed.
+	<-make(chan struct{})
+	return
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == discBotID {
+		return
+	}
+
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
+
 
 func main(){
-	initialize()
-	updateInformation()
+	flag.Parse()
+	//initializeSheets()
+	//updateInformation()
 	
-
+	initializeDiscord()
 }
 
 func updateInformation(){
