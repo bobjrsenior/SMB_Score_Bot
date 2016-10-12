638aa946b8995afb4e755751ee4cbb086e6c377a
bobjrsenior <rcarpe31@uncc.edu>
2016-10-11 20:11:55 -0400

    Made parsing a section into a method to avoid a lot more copy pasting. Added cases/methods for time/score for smb1/smb2/smbd. Alt times for ignored on purpose for now. Story hasn't been added yet.

--- scoreBot.go
+++ scoreBot.go
@@ -19,11 +19,14 @@ import (
 
 type Record struct{
 	Index int
+	Game string
 	Name string
 	Holder string
 	Time string
 	IsTime bool
 }
+
+var retrievingData bool
 
 var records map[string][]Record
 var conf *jwt.Config
@@ -122,21 +125,20 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 func main(){
 	flag.Parse()
-	//initializeSheets()
-	//updateInformation()
+	retrievingData = false
+	initializeSheets()
+	updateInformation()
 	
-	initializeDiscord()
+	//initializeDiscord()
 }
 
 func updateInformation(){
+	retrievingData = true;
 	svc, err := sheets.New(client)
 	if err != nil {
 		log.Fatalf("Unable to create Sheets service: %v", err)
 	}
-	//SheetID: 1wECFB9MpiXswANJpyVSTdozncwwebO2NVP9o4_7FsKk
-	
-	//valueRequestStruct SpreadsheetsValuesGetCall
-	
+
 	// Call for the SMB IL Spreadsheet
 	getCall := svc.Spreadsheets.Get("1KoneeqJzheHFYapQ_JfyxL9sI0X8_BE7ZEVMZt0t0bI")
 	if getCall == nil{
@@ -168,42 +170,145 @@ func updateInformation(){
 				case "SMB1 Time":
 					parseSMB1Time(data)
 				break
+				case "SMB1 Score":
+					parseSMB1Score(data)
+				break
+				case "SMB2 Challenge Time":
+					parseSMB2Time(data)
+				break
+				case "SMB2 Challenge Score":
+					parseSMB2Score(data)
+				break
+				case "SMBDX Challenge Time":
+					parseSMBDTime(data)
+				break
+				case "SMBD Challenge Score":
+					parseSMBDScore(data)
+				break
 			}
-			/*for k, rowData := range data.RowData {
-				fmt.Printf("ROW: %d", k)
-				for _, value := range rowData.Values {
-					fmt.Printf("Value: %s\n", value.FormattedValue)
-				}
-			}*/
 		}
 	}
+	
+	retrievingData = false
 }
 
 func parseSMB1Time(data *sheets.GridData) {
 	//Store the row data and the key for this category
 	rowData := data.RowData
+	parseSection(rowData,"SMB1BeginnerTime", "SMB1", 3, 2, 10, true)
+	parseSection(rowData,"SMB1BeginnerExtraTime", "SMB1", 18, 2, 3, true)
+	parseSection(rowData,"SMB1BeginnerTimeAlt", "SMB1", 7, 22, 1, true)
 	
-	// Work on Beginner Time Winners
-	mapKey := "SMB1BeginnerTime"
+	parseSection(rowData,"SMB1AdvancedTime", "SMB1", 3, 7, 30, true)
+	parseSection(rowData,"SMB1AdvancedExtraTime", "SMB1", 38, 7, 5, true)
+	parseSection(rowData,"SMB1AdvancedTimeAlt", "SMB1", 13, 22, 5, true)
+	
+	parseSection(rowData,"SMB1ExpertTime", "SMB1", 3, 12, 50, true)
+	parseSection(rowData,"SMB1ExpertExtraTime", "SMB1", 58, 12, 10, true)
+	parseSection(rowData,"SMB1ExpertTimeAlt", "SMB1", 24, 22, 5, true)
+	
+	parseSection(rowData,"SMB1MasterTime", "SMB1", 3, 17, 10, true)
+}
+
+func parseSMB1Score(data *sheets.GridData) {
+	//Store the row data and the key for this category
+	rowData := data.RowData
+	parseSection(rowData,"SMB1BeginnerScore", "SMB1", 3, 2, 10, false)
+	parseSection(rowData,"SMB1BeginnerExtraScore", "SMB1", 18, 2, 3, false)
+	
+	parseSection(rowData,"SMB1AdvancedScore", "SMB1", 3, 7, 30, false)
+	parseSection(rowData,"SMB1AdvancedExtraScore", "SMB1", 38, 7, 5, false)
+	
+	parseSection(rowData,"SMB1ExpertScore", "SMB1", 3, 12, 50, false)
+	parseSection(rowData,"SMB1ExpertExtraScore", "SMB1", 58, 12, 10, false)
+	
+	parseSection(rowData,"SMB1MasterScore", "SMB1", 3, 17, 10, false)
+}
+
+// Finish Alts
+func parseSMB2Time(data *sheets.GridData) {
+	//Store the row data and the key for this category
+	rowData := data.RowData
+	parseSection(rowData,"SMB2BeginnerTime", "SMB2", 3, 2, 10, true)
+	parseSection(rowData,"SMB2BeginnerExtraTime", "SMB2", 18, 2, 10, true)
+	
+	parseSection(rowData,"SMB2AdvancedTime", "SMB2", 3, 7, 30, true)
+	parseSection(rowData,"SMB2AdvancedExtraTime", "SMB2", 38, 7, 10, true)
+	
+	parseSection(rowData,"SMB2ExpertTime", "SMB2", 3, 12, 50, true)
+	parseSection(rowData,"SMB2ExpertExtraTime", "SMB2", 58, 12, 10, true)
+	
+	parseSection(rowData,"SMB2MasterTime", "SMB2", 3, 17, 10, true)
+	parseSection(rowData,"SMB2MasterExtraTime", "SMB2", 19, 17, 10, true)
+}
+
+func parseSMB2Score(data *sheets.GridData) {
+	//Store the row data and the key for this category
+	rowData := data.RowData
+	parseSection(rowData,"SMB2BeginnerScore", "SMB2", 3, 2, 10, false)
+	parseSection(rowData,"SMB2BeginnerExtraScore", "SMB2", 3, 2, 10, false)
+	
+	parseSection(rowData,"SMB2AdvancedScore", "SMB2", 3, 7, 30, false)
+	parseSection(rowData,"SMB2AdvancedExtraScore", "SMB2", 38, 7, 10, false)
+	
+	parseSection(rowData,"SMB2ExpertScore", "SMB2", 3, 12, 50, false)
+	parseSection(rowData,"SMB2ExpertExtraScore", "SMB2", 58, 12, 10, false)
+	
+	parseSection(rowData,"SMB2MasterScore", "SMB2", 3, 17, 10, false)
+	parseSection(rowData,"SMB2MasterExtraScore", "SMB2", 19, 17, 10, false)
+	
+}
+
+// Finish Alts
+func parseSMBDTime(data *sheets.GridData) {
+	//Store the row data and the key for this category
+	rowData := data.RowData
+	parseSection(rowData,"SMBDBeginnerTime", "SMBD", 3, 2, 40, true)
+	parseSection(rowData,"SMBDBeginnerExtraTime", "SMBD", 48, 2, 20, true)
+	
+	parseSection(rowData,"SMBDAdvancedTime", "SMBD", 3, 7, 70, true)
+	parseSection(rowData,"SMBDAdvancedExtraTime", "SMBD", 78, 7, 20, true)
+	
+	parseSection(rowData,"SMBDExpertTime", "SMBD", 3, 12, 100, true)
+	parseSection(rowData,"SMBDExpertExtraTime", "SMBD", 108, 12, 20, true)
+	
+	parseSection(rowData,"SMBDMasterTime", "SMBD", 3, 17, 12, true)
+	parseSection(rowData,"SMBDMasterExtraTime", "SMBD", 28, 17, 10, true)
+}
+
+func parseSMBDScore(data *sheets.GridData) {
+	//Store the row data and the key for this category
+	rowData := data.RowData
+	parseSection(rowData,"SMBDBeginnerScore", "SMBD", 3, 2, 40, false)
+	parseSection(rowData,"SMBDBeginnerExtraScore", "SMBD", 3, 48, 20, false)
+	
+	parseSection(rowData,"SMBDAdvancedScore", "SMBD", 3, 7, 70, false)
+	parseSection(rowData,"SMBDAdvancedExtraScore", "SMBD", 78, 7, 20, false)
+	
+	parseSection(rowData,"SMBDExpertScore", "SMBD", 3, 12, 100, false)
+	parseSection(rowData,"SMBDExpertExtraScore", "SMBD", 108, 12, 20, false)
+	
+	parseSection(rowData,"SMBDMasterScore", "SMBD", 3, 17, 12, false)
+	parseSection(rowData,"SMBDMasterExtraScore", "SMBD", 28, 17, 10, false)
+}
+
+func parseSection(rowData []*sheets.RowData, mapKey string, game string, startRow int, startCol int, amount int, isTime bool){
+	endRow := startRow + amount
+	
+	// Work on Advanced Time Winners
 	records[mapKey] = make([]Record, 0, 250)
-	records[mapKey] = append(records[mapKey], Record{Index: 1, Name: "", Time: "", IsTime: false})
+	records[mapKey] = append(records[mapKey], Record{Index: 1, Game: game, Name: "", Holder: "", Time: "", IsTime: isTime})
 	currentIndex := 1
 	
-	// Retrieve Beginner Time Winners
-	for i := 3; i < 13; i++ {
-		name := rowData[i].Values[2].FormattedValue
-		time := rowData[i].Values[3].FormattedValue
-		isTime := true
-		records[mapKey] = append(records[mapKey], Record{Index: currentIndex, Name: name, Time: time, IsTime: isTime})
+	for i := startRow; i < endRow; i++ {
+		name := rowData[i].Values[startCol].FormattedValue
+		time := rowData[i].Values[startCol + 1].FormattedValue
+		holder := rowData[i].Values[startCol + 2].FormattedValue
+		records[mapKey] = append(records[mapKey], Record{Index: currentIndex, Game: game, Name: name, Holder: holder, Time: time, IsTime: isTime})
 		
 		currentIndex++
 	}
 	records[mapKey][0].Index = currentIndex
-	
-	// Print information for manual checking
-	for _, record := range records[mapKey] {
-		fmt.Printf("%d: %s\n", record.Index, record.Name)
-	}
 }
 
 func valueOrFileContents(value string, filename string) string {
